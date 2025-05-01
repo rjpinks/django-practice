8f73751371ca0308c443b1f9312df25cfdcc7875
Ryan Pinkston <pinkstonr54@gmail.com>
2025-05-01 17:23:44 -0500

    made page title dynamic

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
 	# ex: /blog/
-	path('', views.index, name='index')
+	path('', views.index, name='index'),
+	# ex: /blog/posts
+	path('posts/', views.posts, name='posts'),
+	# ex: /blog/posts/art
+	path('posts/<slug:genre>', views.posts_genre, name='posts_genre')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
+from .models import Post
+
 
 def index(request):
-	return render(request, 'blog/index.html')
+	post_genres = Post.objects.values('genre').distinct()
+	return render(request, 'blog/index.html', {'posts': post_genres})
 
+
+def posts(request):
+	posts = Post.objects.all()
+	return render(request, 'blog/posts.html', {'posts': posts})
+
+
+def posts_genre(request):
+	posts = Post.objects.filter(genre=request.genre)
+	return render(request, 'blog/posts.html', {'posts': posts})
+	
